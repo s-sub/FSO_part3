ba0b0b797916f32381236a501b6af164b49a9b7b
s-sub <shiv97@gmail.com>
2022-07-27 18:31:55 -0400

    Committing to Heroku with frontend added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Seperate git repo for part 3 of FSO.
+See link to server here: https://stark-spire-58031.herokuapp.com/api/persons
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
     return [
       tokens.method(req, res),
