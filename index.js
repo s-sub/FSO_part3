d60cd7b6cf2be9db181c26a7a674b7a6464c91c3
s-sub <shiv97@gmail.com>
2022-07-28 14:45:38 -0400

    Part 3 completed in full. Final version also hosted on Heroku site

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body)
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
@@ -30,49 +30,49 @@ const errorHandler = (error, request, response, next) => {
   }
   else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-  } 
+  }
 
   next(error)
-} 
+}
 app.use(errorHandler)
 
 
 
 let persons =
     [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
+      {
+        'id': 1,
+        'name': 'Arto Hellas',
+        'number': '040-123456'
+      },
+      {
+        'id': 2,
+        'name': 'Ada Lovelace',
+        'number': '39-44-5323523'
+      },
+      {
+        'id': 3,
+        'name': 'Dan Abramov',
+        'number': '12-43-234345'
+      },
+      {
+        'id': 4,
+        'name': 'Mary Poppendieck',
+        'number': '39-23-6423122'
+      }
     ]
 
 // app.get('/', (request, response) => {
 //     response.send('<h1>Hello World!</h1>')
 // })
-    
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
     response.json(people)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -84,48 +84,47 @@ app.get('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 
   // const id = Number(request.params.id)
-    // const person = persons.find(person => {
-    //     return person.id === id
-    // })
-    // if (person) {
-    //     response.json(person)
-    //   } else {
-    //     response.status(404).end()
-    //   }
+  // const person = persons.find(person => {
+  //     return person.id === id
+  // })
+  // if (person) {
+  //     response.json(person)
+  //   } else {
+  //     response.status(404).end()
+  //   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
-  
-    // const id = Number(request.params.id)
-    // persons = persons.filter(person => person.id !== id)
-  
-    // response.status(204).end()
-  })
 
-app.post('/api/persons/', (request, response, next) => {
-  // FIX ERROR HANDLING FROM FRONTEND -> re-build and copy to backend root
-    const body = request.body
-    if (!body.name || !body.number) {
-        return response.status(400).json({error: 'Name or number missing'})
-    }
-    else {
-      const person = new Person({
-        name: body.name,
-        number: body.number 
-      })
-      person.save().then(savedPerson => {
-        response.json(savedPerson)
-      })
-      .catch(error => next(error))
-    }
+  // const id = Number(request.params.id)
+  // persons = persons.filter(person => person.id !== id)
+
+  // response.status(204).end()
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'Name or number missing' })
+  }
+  else {
+    const person = new Person({
+      name: body.name,
+      number: body.number
+    })
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+      .catch(error => next(error))
+  }
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
   const person = {
     name: body.name,
@@ -139,11 +138,11 @@ app.put('/api/persons/:id', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const info = {
-        contacts: persons.length,
-        date: new Date()
-    }
-    response.send(`<p>Phonebook has info for ${info.contacts} people<p><p>${info.date}</p>`)
+  const info = {
+    contacts: persons.length,
+    date: new Date()
+  }
+  response.send(`<p>Phonebook has info for ${info.contacts} people<p><p>${info.date}</p>`)
 })
 
 const PORT = process.env.PORT || 3001
