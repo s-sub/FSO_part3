c8b83255556f6fe6326330cd7f70d41fa9ce8562
s-sub <shiv97@gmail.com>
2022-07-28 14:22:27 -0400

    Fixing root page of app

--- index.js
+++ index.js
@@ -62,9 +62,9 @@ let persons =
         }
     ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World!</h1>')
+// })
     
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
